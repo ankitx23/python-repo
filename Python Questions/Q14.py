a600a319b9172d35d6569ff411aa6fe51fbe8071
ankitx23 <ankitxydv@gmail.com>
2025-08-28 23:05:39 -0400

    new

---
+++ Python Questions/Q14.py
@@ -0,0 +1,13 @@
+# Exercise 14: Print a downward half-pyramid pattern of stars
+# * * * * *  
+# * * * *  
+# * * *  
+# * *  
+# *
+
+a = ("*")
+
+for a in range(1,6):
+    for b in range (1,5):
+        print ("*")
+    print()
