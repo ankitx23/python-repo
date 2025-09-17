282b12c9b6e59594b5856e2700ba8cafccfd24b0
ankitx23 <ankitxydv@gmail.com>
2025-09-17 12:51:45 -0400

    Qs

--- Python Questions/Q14.py
+++ Python Questions/Q14.py
@@ -5,9 +5,7 @@
 # * *  
 # *
 
-a = ("*")
-
-for a in range(1,6):
-    for b in range (1,5):
-        print ("*")
+for i in range(5, 0, -1):
+    for j in range(i):
+        print ("*" , end= " ")
     print()
