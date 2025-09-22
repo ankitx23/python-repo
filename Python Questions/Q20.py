4eb807b94053ce08b5db42d02932cecc02d04c6e
ankitx23 <ankitxydv@gmail.com>
2025-09-22 00:18:35 -0400

    update

--- Python Questions/Q20.py
+++ Python Questions/Q20.py
@@ -7,3 +7,21 @@
 # Enter a string: PYnative
 # The string does not contain any digits.
 
+# a=(0,1,2,3,4,5,6,7,8,9,)
+# print (type(b), b)
+found = False
+
+str = input("Enter the word")
+
+# str = str(input("Enter the Word"))
+
+for ch in str:
+    if ch in "0123456789":
+        found=True
+        break
+
+if found:
+    print("it contains numbers")  
+else:
+    print("it does not contain numbers")
+
